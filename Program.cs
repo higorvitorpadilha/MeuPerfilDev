5ef311505b599c758932c96a981adcad8ce219d0
Higor <higorvitorpadilha@gmail.com>
2024-03-24 12:04:37 -0300

    Adicao de novos arquivos

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Digite seu nome. ");
+var nome = Console.ReadLine(); 
+Console.WriteLine(nome);
+
+Console.WriteLine("Digite sua bio. ");
+var bio = Console.ReadLine(); 
+Console.WriteLine(bio);
+
+Console.WriteLine("Digite seu pais.");
+var pais = Console.ReadLine();
+Console.WriteLine(pais);
+
+Console.WriteLine("---- Processando seu perfil -----");
+
+Console.WriteLine($"Olá, me chamo {nome}, Bio {bio}, Moro no pais {pais}");
